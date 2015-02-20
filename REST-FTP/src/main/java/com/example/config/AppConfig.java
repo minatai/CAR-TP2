1f18c3e86d601025c8ad0c9a10462ad792abeb20
arctarus44 <arthur.dewarumez@gmail.com>
2015-02-20 11:57:09 +0100

    Ajout de toutes les classes après corrections et reformatage.

---
+++ REST-FTP/src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,65 @@
+package main.java.com.example.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.ext.RuntimeDelegate;
+
+import main.java.car.HelloWorldResource;
+import main.java.com.example.rs.JaxRsApiApplication;
+import main.java.com.example.rs.PeopleRestService;
+import main.java.com.example.services.PeopleService;
+
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class AppConfig {
+	@Bean(destroyMethod = "shutdown")
+	public SpringBus cxf() {
+		return new SpringBus();
+	}
+
+	@Bean
+	@DependsOn("cxf")
+	public Server jaxRsServer() {
+		final JAXRSServerFactoryBean factory = RuntimeDelegate.getInstance()
+				.createEndpoint(jaxRsApiApplication(),
+						JAXRSServerFactoryBean.class);
+
+		final List<Object> serviceBeans = new ArrayList<Object>();
+		// serviceBeans.add(peopleRestService());
+		serviceBeans.add(new HelloWorldResource());
+
+		factory.setServiceBeans(serviceBeans);
+		factory.setAddress("/" + factory.getAddress());
+		factory.setProviders(Arrays.<Object> asList(jsonProvider()));
+		return factory.create();
+	}
+
+	@Bean
+	public JaxRsApiApplication jaxRsApiApplication() {
+		return new JaxRsApiApplication();
+	}
+
+	@Bean
+	public PeopleRestService peopleRestService() {
+		return new PeopleRestService();
+	}
+
+	@Bean
+	public PeopleService peopleService() {
+		return new PeopleService();
+	}
+
+	@Bean
+	public JacksonJsonProvider jsonProvider() {
+		return new JacksonJsonProvider();
+	}
+}

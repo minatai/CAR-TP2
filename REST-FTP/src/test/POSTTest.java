80d3be23e9d01282cb0ccc31d89b807f10144559
arctarus44 <arthur.dewarumez@gmail.com>
2015-03-19 20:34:50 +0100

    Implémentation test.

--- REST-FTP/src/test/POSTTest.java
+++ REST-FTP/src/test/POSTTest.java
@@ -12,35 +12,46 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 /**
+ * This class test every ressources reachable with POST HTTP request
+ *
  * @author arctarus
  *
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class POSTTest extends BaseTesting {
 
-	private static int i = 0;
+	private final String DIR_NAME = "dirname";
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * Try to create a directory.
+	 */
 	@Test
 	public void testCreateDirectoryOK() {
 		HttpResponse response = null;
 		try {
-			response = this.createDirectory("Test");
+			response = this.createDirectory(this.DIR_NAME, this.CORRECT_LOGIN,
+					this.CORRECT_PASSWORD);
 		} catch (final IOException e) {
 			e.printStackTrace();
 			fail("Une erreur est intervenu durant le test");
 		}
+		System.out.println(response.getStatusLine().getStatusCode());
 		assertTrue(response.getStatusLine().getStatusCode() == 200);
 	}
 
+	/**
+	 * Try to create an directory when another with the same name exists.
+	 */
 	@Test
 	public void testCreateDirectorySameTwice() {
 		HttpResponse response = null;
 		try {
-			response = this.createDirectory("Test");
+			response = this.createDirectory(this.DIR_NAME, this.CORRECT_LOGIN,
+					this.CORRECT_PASSWORD);
 		} catch (final IOException e) {
 			e.printStackTrace();
 			fail("Une erreur est intervenu durant le test");
@@ -48,11 +59,31 @@ public class POSTTest extends BaseTesting {
 		assertTrue(response.getStatusLine().getStatusCode() == 403);
 	}
 
+	/**
+	 * Try to create a directory without proper couple user/password.
+	 */
+	@Test
+	public void testCreateDirectoryUnauthorized() {
+		HttpResponse response = null;
+		try {
+			response = this.createFile(this.DIR_NAME, this.INCORRECT_LOGIN,
+					this.INCORRECT_PASSWORD);
+		} catch (final IOException e) {
+			e.printStackTrace();
+			fail("Une erreur est intervenu durant le test");
+		}
+		// assertTrue(response.getStatusLine().getStatusCode() == 401);
+	}
+
+	/**
+	 * Try to create a file.
+	 */
 	@Test
 	public void testCreateFileOK() {
 		HttpResponse response = null;
 		try {
-			response = this.createFile("fichier");
+			response = this.createFile("fichier", this.CORRECT_LOGIN,
+					this.CORRECT_PASSWORD);
 		} catch (final IOException e) {
 			e.printStackTrace();
 			fail("Une erreur est intervenu durant le test");
@@ -60,18 +91,20 @@ public class POSTTest extends BaseTesting {
 		assertTrue(response.getStatusLine().getStatusCode() == 200);
 	}
 
+	/**
+	 *
+	 */
 	@Test
-	public void testCreateFileUnauthorise() {
-		// HttpResponse response = null;
-		// try {
-		// response = this.createFile("fichier");
-		// } catch (final IOException e) {
-		// e.printStackTrace();
-		// fail("Une erreur est intervenu durant le test");
-		// }
-		// System.out.println(response.getStatusLine().getStatusCode());
-		// assertTrue(response.getStatusLine().getStatusCode() == 403);
-		// TODO trouvé une solution élégante pour déclencher le code 403.
-		return;
+	public void testCreateFileUnauthorized() {
+		HttpResponse response = null;
+		try {
+			response = this.createFile("fichier", this.INCORRECT_LOGIN,
+					this.INCORRECT_PASSWORD);
+		} catch (final IOException e) {
+			e.printStackTrace();
+			fail("Une erreur est intervenu durant le test");
+		}
+		System.out.println(response.getStatusLine().getStatusCode());
+		assertTrue(response.getStatusLine().getStatusCode() == 401);
 	}
 }
